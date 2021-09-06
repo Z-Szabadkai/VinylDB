62c8a2264e0ecb4be9ce7038d0d11ae27d8e8fc6
Z-Szabadkai <zoltan.szabadkai@gmail.com>
2021-09-06 19:19:49 +0200

    Fields to Album class added

--- src/main/java/application/models/Album.java
+++ src/main/java/application/models/Album.java
@@ -1,4 +1,84 @@
 package application.models;
 
+import java.util.Date;
+import java.util.List;
+
 public class Album {
+
+    private String title;
+    private Date releaseDate;
+    private String label;
+    private List<String> genre;
+    private List<String> style;
+    private Ltd_Release limited;
+    private String color;
+
+    public Album(String title, Date releaseDate, String label, List<String> genre, List<String> style, Ltd_Release limited, String color) {
+        this.title = title;
+        this.releaseDate = releaseDate;
+        this.label = label;
+        this.genre = genre;
+        this.style = style;
+        this.limited = limited;
+        this.color = color;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(Date releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public List<String> getGenre() {
+        return genre;
+    }
+
+    public void setGenre(List<String> genre) {
+        this.genre = genre;
+    }
+
+    public List<String> getStyle() {
+        return style;
+    }
+
+    public void setStyle(List<String> style) {
+        this.style = style;
+    }
+
+    public Ltd_Release getLimited() {
+        return limited;
+    }
+
+    public void setLimited(Ltd_Release limited) {
+        this.limited = limited;
+    }
+
+    public String getColor() {
+        if (getLimited().equals(Ltd_Release.BLACK)) {
+            return "black";
+        }
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
 }
